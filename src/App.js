e1d404e22d6f00b76c4d98cbd424589e48248bc3
adonis-sampaio <adonissamp@gmail.com>
2021-01-10 17:29:33 -0200

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Movie from './components/Movie';
+import {useEffect, useState} from 'react';
+
+
+const FEATURED_API = "https://api.themoviedb.org/3/movie/popular?api_key=8226a08b470ebbe92062c043cd6759cf&language=en-US&page=1";
+const IMG_API = 'https://image.tmdb.org/t/p/w180';
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=8226a08b470ebbe92062c043cd6759cf&language=en-US&page=1&include_adult=false"
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(async () => {
+    const moviesResp = await fetch(FEATURED_API);
+    const moviesR = await moviesResp.json();
+
+    setMovies(moviesR);
+  },[])
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map(movie => (
+          <Movie />
+        ))}
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Movie() {
+    return (
+        <div className="movie">
+            This is a movie Component
+        </div>
+    )
+}
+
+export default Movie
